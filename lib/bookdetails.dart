6fb1601e590cd8bce1be8b1eebd5eede27660dc5
abdelaziz-belhachmi <abdelaziz.belhachmi@etu.uae.ac.ma>
2024-04-12 23:31:26 +0000

    book details

--- lib/bookdetails.dart
+++ lib/bookdetails.dart
@@ -1,19 +1,80 @@
 import 'package:flutter/material.dart';
+import 'GestionLivres.dart';
+import 'dart:io';
 
+
+// class BookDetailsPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Book Details",style: TextStyle(color: Color.fromRGBO(255, 255,255,1),fontSize: 25),),
+//         backgroundColor: Color.fromARGB(255, 43, 44, 68),
+//       ),
+//       body: Center(
+//         child: Text('Book Details Page Content'),
+//       ),
+//     );
+//   }
+// }
+//
 
 class BookDetailsPage extends StatelessWidget {
+  final Book book;
+
+  const BookDetailsPage({Key? key, required this.book}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Book Details",style: TextStyle(color: Color.fromRGBO(255, 255,255,1),fontSize: 25),),
+        title: Text("Book Details", style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1), fontSize: 25),),
         backgroundColor: Color.fromARGB(255, 43, 44, 68),
       ),
-      body: Center(
-        child: Text('Book Details Page Content'),
-      ),
+      body:Container(
+        color: Color.fromARGB(255, 43, 44, 68),
+      child:ListView(
+        children: [
+          Container(
+            width: double.infinity, // Take 100% width
+            height: MediaQuery.of(context).size.height * 0.6, // Take 60% height of the screen
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: FileImage(File(book.imagePath)),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 20), // Add some space between the image and text
+          Text('Title: ${book.title}', style: TextStyle(color: Colors.white,fontSize: 25)),
+          Text('Author: ${book.author}', style: TextStyle(color: Colors.white,fontSize: 25)),
+          Text('Category: ${book.category}', style: TextStyle(color: Colors.white,fontSize: 25)),
+          Text('Publication Year: ${book.publicationYear}', style: TextStyle(color: Colors.white,fontSize: 25)),
+          Text('Available Copies: ${book.availableCopies}', style: TextStyle(color: Colors.white,fontSize: 25)),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Handle reserve button pressed
+                },
+                child: Text('Reserve'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle emprainte a copy button pressed
+                },
+                child: Text('Emprainte a Copy'),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+
+        ],
+
+
+      ),),
     );
   }
 }
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,6 @@ import 'package:path/path.dart';
 
 String imagePath = 'assets/bookcases-1869616_640.jpg';
 
-// void main() => runApp(App2());
-
-// class App2 extends StatelessWidget {
-//   final appTitle = 'Library';
-//   final BookDataProvider dataprovider = BookDataProvider();
-//   late final BookRepository bookRepository;
-//
-//   App2({Key? key}) : super(key: key) {
-//     bookRepository = BookRepository(dataProvider: dataprovider);
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: appTitle,
-//       home: MyHomePage(title: appTitle, bookRepository: bookRepository),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure that the binding is initialized
@@ -58,101 +38,217 @@ class App2 extends StatelessWidget {
     );
   }
 }
+
+//
 // class MyHomePage extends StatelessWidget {
 //   final String title;
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//   final BookRepository bookRepository; // Add this field
+//
+//   const MyHomePage({Key? key, required this.title, required this.bookRepository}) : super(key: key);
+//
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
-//         title: Text(title,style: TextStyle(color: Color.fromRGBO(255, 255,255,1),fontSize: 25),),
+//         title: Text(
+//           title,
+//           style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1), fontSize: 25),
+//         ),
 //         backgroundColor: Color.fromARGB(255, 43, 44, 68),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.refresh,color: Colors.white,),
+//             onPressed: null, // Call _refreshData when button is pressed
+//           ),
+//         ],
 //       ),
 //       body: SafeArea(
 //         child: Container(
-//           color: Color.fromRGBO(34,34,48,1),
-//           // height: MediaQuery.of(context).size.height * 0.9, // 90% of parent's height
-//           // width: MediaQuery.of(context).size.width, // full width of parent
-//           child: ListWheelScrollView(
-//             // scrollDirection: Axis.horizontal, // Set horizontal direction
-//             diameterRatio: 7.0, // Adjust this value to set the perspective
-//
-//             itemExtent: MediaQuery.of(context).size.height*0.8, // 90% of parent's width
-//             children: List.generate(3, (index) {
-//               return GestureDetector(
-//                 onTap: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(builder: (context) => BookDetailsPage()),
-//                   );
-//                 },
-//                 child: Container(
-//                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(10),
-//                     image: DecorationImage(
-//                       image: AssetImage(imagePath),
-//                       fit: BoxFit.cover,
-//                     ),
-//                   ),
-//                   child: Align(
-//                     alignment: Alignment.bottomCenter,
-//                     child: Padding(
-//                       padding: EdgeInsets.all(10),
-//                       child: Column(
-//                         mainAxisAlignment: MainAxisAlignment.end,
-//
-//                         children: [
-//                           Text('Book Title $index',
-//                             style: TextStyle(
-//                               color: Colors.white,
-//                               fontSize: 26,
-//                               fontWeight: FontWeight.bold,
+//           color: Color.fromRGBO(34, 34, 48, 1),
+//           child: FutureBuilder<List<Book>>(
+//             future: bookRepository.getBooks(), // Fetch books from the repository
+//             builder: (context, snapshot) {
+//               if (snapshot.connectionState == ConnectionState.waiting) {
+//                 return Center(child: CircularProgressIndicator());
+//               } else if (snapshot.hasError) {
+//                 return Center(child: Text('Error: ${snapshot.error}'));
+//               } else {
+//                 return ListWheelScrollView(
+//                   diameterRatio: 7.0,
+//                   itemExtent: MediaQuery.of(context).size.height * 0.8,
+//                   children: snapshot.data!.map((book) {
+//                     return GestureDetector(
+//                       onTap: () {
+//                         Navigator.push(
+//                           context,
+//                           MaterialPageRoute(builder: (context) => BookDetailsPage()),
+//                         );
+//                       },
+//                       child: Container(
+//                         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.circular(10),
+//                           image: DecorationImage(
+//                             image: FileImage(File(book.imagePath)),
+//                             fit: BoxFit.cover,
+//                           ),
+//                         ),
+//                         child: Align(
+//                           alignment: Alignment.bottomCenter,
+//                           child: Padding(
+//                             padding: EdgeInsets.all(10),
+//                             child: Column(
+//                               mainAxisAlignment: MainAxisAlignment.end,
+//                               children: [
+//                                 Text(
+//                                   book.title,
+//                                   style: TextStyle(
+//                                     color: Colors.white,
+//                                     fontSize: 26,
+//                                     fontWeight: FontWeight.bold,
+//                                   ),
+//                                 ),
+//                                 Text(
+//                                   book.author,
+//                                   style: TextStyle(
+//                                     color: Colors.white,
+//                                     fontSize: 19,
+//                                     fontWeight: FontWeight.bold,
+//                                   ),
+//                                 )
+//                               ],
 //                             ),
 //                           ),
-//                           //
-//                           Text('Book Auteur $index',
-//                             style: TextStyle(
-//                               color: Colors.white,
-//                               fontSize: 19,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           )
-//                           //
-//                         ],
+//                         ),
 //                       ),
-//                     ),
-//                   ),
-//                 ),
-//               );
-//             }),
+//                     );
+//                   }).toList(),
+//                 );
+//               }
+//             },
 //           ),
 //         ),
 //       ),
 //
+//       //
+//       //
+//       drawer: Drawer(
+//         backgroundColor: Color.fromRGBO(34, 34, 48, 1),
+//         child: ListView(
 //
+//           padding: const EdgeInsets.all(0),
+//           children: [
+//             DrawerHeader(
+//               decoration: BoxDecoration(
+//                 color: Color.fromRGBO(34, 34, 48, 1),
+//                 image: DecorationImage(
+//                   image: AssetImage(imagePath),
+//                   fit: BoxFit.cover,
+//                 ),
+//               ),
+//               child: null,
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.person),
+//               title: const Text(' Reservations ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => reservationsPage()),
+//                 );
+//               },
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.book),
+//               title: const Text(' Mes Empreintes ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => MesEmpreintesPage()),
+//                 );
+//               },
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.video_label),
+//               title: const Text(' Retours ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => RetoursPage()),
+//                 );
+//               },
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.edit),
+//               title: const Text(' Gestion Livres ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 Navigator.push(
+//                   context,
+//                   // u need to create the bookrepository instance before calling it in the main or it wont work
+//                   MaterialPageRoute(builder: (context) => GestionLivres(bookRepository: BookDataProvider().init().then((dataProvider) => BookRepository(dataProvider: dataProvider)),),),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+// }
 
-class MyHomePage extends StatelessWidget {
+
+class MyHomePage extends StatefulWidget {
   final String title;
-  final BookRepository bookRepository; // Add this field
+  final BookRepository bookRepository;
 
-  const MyHomePage({Key? key, required this.title, required this.bookRepository}) : super(key: key);
+  const MyHomePage({Key? key, required this.title, required this.bookRepository})
+      : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<Book>> _booksFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  Future<void> _refreshData() async {
+    setState(() {
+      _booksFuture = widget.bookRepository.getBooks();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          title,
+          widget.title,
           style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1), fontSize: 25),
         ),
         backgroundColor: Color.fromARGB(255, 43, 44, 68),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh, color: Colors.white),
+            onPressed: _refreshData, // Call _refreshData when button is pressed
+          ),
+        ],
       ),
       body: SafeArea(
         child: Container(
           color: Color.fromRGBO(34, 34, 48, 1),
           child: FutureBuilder<List<Book>>(
-            future: bookRepository.getBooks(), // Fetch books from the repository
+            future: _booksFuture, // Use the updated _booksFuture
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return Center(child: CircularProgressIndicator());
@@ -167,7 +263,9 @@ class MyHomePage extends StatelessWidget {
                       onTap: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => BookDetailsPage()),
+                          // MaterialPageRoute(builder: (context) => BookDetailsPage()),
+                          MaterialPageRoute(builder: (context) => BookDetailsPage(book: book)),
+
                         );
                       },
                       child: Container(
@@ -215,13 +313,9 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
-
-      //
-      //
       drawer: Drawer(
         backgroundColor: Color.fromRGBO(34, 34, 48, 1),
         child: ListView(
-
           padding: const EdgeInsets.all(0),
           children: [
             DrawerHeader(
@@ -236,7 +330,7 @@ class MyHomePage extends StatelessWidget {
             ),
             ListTile(
               leading: const Icon(Icons.person),
-              title: const Text(' Reservations ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+              title: const Text(' Reservations ', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.push(
@@ -247,7 +341,7 @@ class MyHomePage extends StatelessWidget {
             ),
             ListTile(
               leading: const Icon(Icons.book),
-              title: const Text(' Mes Empreintes ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+              title: const Text(' Mes Empreintes ', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.push(
@@ -258,7 +352,7 @@ class MyHomePage extends StatelessWidget {
             ),
             ListTile(
               leading: const Icon(Icons.video_label),
-              title: const Text(' Retours ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+              title: const Text(' Retours ', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.push(
@@ -269,12 +363,11 @@ class MyHomePage extends StatelessWidget {
             ),
             ListTile(
               leading: const Icon(Icons.edit),
-              title: const Text(' Gestion Livres ',style: TextStyle(color: Color.fromRGBO(255,255,255, 1)),),
+              title: const Text(' Gestion Livres ', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.push(
                   context,
-                  // u need to create the bookrepository instance before calling it in the main or it wont work
                   MaterialPageRoute(builder: (context) => GestionLivres(bookRepository: BookDataProvider().init().then((dataProvider) => BookRepository(dataProvider: dataProvider)),),),
                 );
               },
